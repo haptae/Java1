b1072706baecfb0abec6caaeb19eec819fe18f7b
haptae <122253382+haptae@users.noreply.github.com>
2023-01-11 22:59:02 +0900

    Add files via upload
    변수
    
    integer 정수
    real number->double 실수
    string 문자열
    
    system.out.println(f.getclass()); f가 어떤 데이터 type인지 알려줌
    
    sysout ctrl+space --> system.out.println

---
+++ First/src/first/StringAPP.java
@@ -0,0 +1,22 @@
+package first;
+
+public class StringAPP {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");  //string
+		System.out.println('H');  //character
+		System.out.println("H");
+		
+		System.out.println("Hello"
+				+ "World");             //HelloWorld
+		
+		//new line
+		System.out.println("Hello \nWorld");
+		
+		
+		//escape
+		System.out.println("Hello \"World\"");  //Hello "World"
+	}
+
+}
---
+++ First/src/first/Variable.java
@@ -0,0 +1,17 @@
+package first;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		int a=1; //Number-> integer...-2,-1,0,1,2...
+				System.out.println(a);
+				
+				double b=1.4; //real number->double...-2.3, -1.0, 0, 3,4
+				System.out.println(b);
+				
+				String c= "Hello World";
+				System.out.println(c);
+	}
+
+}
